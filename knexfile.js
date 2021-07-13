4706ea14f892cf9c8326c15192ca564b9c87d3fd
bhawnishk1 <mrbhawnish@gmail.com>
2021-07-12 18:17:14 -0700

    feature/bugfix

--- knexfile.js
+++ knexfile.js
@@ -40,12 +40,13 @@ module.exports = {
   production: {
     client: pg,
     connection: process.env.DATABASE_URL,
+    tableName: 'knex_migrations',
     migrations: {
       directory: './data/migrations'
     },
     seeds: {
-     directory: './data/seeds'
-    }
-  }
+     directory: './data/seeds',
+    },
+  },
 
 };
